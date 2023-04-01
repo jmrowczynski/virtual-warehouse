a19f855845525b2020bedb166cb55a084e7886e8
Jakub Mrówczyński <jmrowczynski12@gmail.com>
2023-04-01 20:14:12 +0200

    add basic login view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,4 +1,30 @@
-<template><div>Login</div></template>
+<template>
+    <section class="min-h-screen flex items-center justify-center flex-col">
+        <div
+            class="card w-full max-w-xl mx-auto bg-base-200/70 shadow-xl px-4 py-8 md:p-12"
+        >
+            <h1 class="text-4xl mb-8 text-center font-semibold">Warehouse</h1>
+            <div>
+                <div class="mb-4">
+                    <input
+                        type="email"
+                        placeholder="example@example.com"
+                        class="input input-bordered input-primary w-full"
+                        aria-label="email"
+                    />
+                </div>
+                <div>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        class="input input-bordered input-primary w-full"
+                        aria-label="password"
+                    />
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
 <script>
 export default {
     name: 'LoginView',
