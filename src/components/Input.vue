d4540f6bf66ef682b07247d8a0a49d22b6be621f
Jakub Mrówczyński <jmrowczynski12@gmail.com>
2023-04-02 20:46:44 +0200

    create Input component

---
+++ src/components/Input.vue
@@ -0,0 +1,31 @@
+<template>
+    <Field
+        class="input input-bordered input-primary w-full"
+        :type="type"
+        :placeholder="placeholder"
+        :aria-label="ariaLabel"
+        :name="name"
+    />
+    <ErrorMessage :name="name" class="text-error inline-block mt-1" />
+</template>
+
+<script setup lang="ts">
+import { Field, ErrorMessage } from 'vee-validate';
+
+interface Props {
+    type?: string;
+    placeholder?: string;
+    ariaLabel?: string;
+    name: string;
+}
+
+withDefaults(defineProps<Props>(), {
+    type: 'text',
+});
+</script>
+
+<script lang="ts">
+export default {
+    name: 'Input',
+};
+</script>
