675af49980b6724be37fdfde7591045d218d3d9c
Jakub Mrówczyński <jmrowczynski12@gmail.com>
2023-04-02 20:47:13 +0200

    add login form with validation

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,29 +3,59 @@
         <div
             class="card w-full max-w-xl mx-auto bg-base-200/70 shadow-xl px-4 py-8 md:p-12"
         >
-            <h1 class="text-4xl mb-8 text-center font-semibold">Warehouse</h1>
-            <div>
+            <h1 class="text-4xl mb-8 text-center font-semibold">Magazyn</h1>
+            <Form
+                @submit="onSubmit"
+                :validation-schema="schema"
+                v-slot="{ meta }"
+            >
                 <div class="mb-4">
-                    <input
+                    <Input
                         type="email"
+                        name="email"
                         placeholder="example@example.com"
-                        class="input input-bordered input-primary w-full"
-                        aria-label="email"
+                        ariaLabel="email"
                     />
                 </div>
-                <div>
-                    <input
+                <div class="mb-8">
+                    <Input
                         type="password"
-                        placeholder="Password"
-                        class="input input-bordered input-primary w-full"
-                        aria-label="password"
+                        placeholder="Hasło"
+                        ariaLabel="password"
+                        name="password"
                     />
                 </div>
-            </div>
+                <button
+                    class="btn w-full"
+                    :class="{ 'btn-disabled': !meta.valid }"
+                >
+                    ZALOGUJ
+                </button>
+            </Form>
         </div>
     </section>
 </template>
-<script>
+
+<script setup lang="ts">
+import { Form } from 'vee-validate';
+import * as Yup from 'yup';
+import Input from '../components/Input.vue';
+
+const schema = Yup.object().shape({
+    email: Yup.string()
+        .email('Nieprawidłowy email')
+        .required('Email jest wymagany')
+        .label('Email'),
+    password: Yup.string().required('Hasło jest wymagane').label('Hasło'),
+});
+
+const onSubmit = (values: { email: string; password: string }) => {
+    // eslint-disable-next-line no-console
+    console.log(values);
+};
+</script>
+
+<script lang="ts">
 export default {
     name: 'LoginView',
 };
