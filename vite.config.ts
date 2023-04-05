d838d91694021c1351453184f811b43aac7030be
Jakub Mrówczyński <jmrowczynski12@gmail.com>
2023-04-05 23:43:36 +0200

    config alias imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    resolve: { alias: { vue: 'vue/dist/vue.esm-bundler.js' } },
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+            '@': path.resolve(__dirname, './src'),
+            '@assets': path.resolve(__dirname, './src/assets'),
+            '@components': path.resolve(__dirname, './src/components'),
+        },
+    },
 });
