db85239dc4e91ad3a4b422bc09c93ce5d49dbccd
Jakub Mrówczyński <jmrowczynski12@gmail.com>
2023-04-05 23:48:15 +0200

    add axios interceptor for csrf cookie

--- src/services/api/axios/index.ts
+++ src/services/api/axios/index.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import AuthApi from '@/services/api/connections/AuthApi';
 
 const config = {
     baseURL: import.meta.env.VITE_APP_API_URL,
@@ -7,3 +8,18 @@ const config = {
 };
 
 export const axiosInstance = axios.create(config);
+
+axiosInstance.interceptors.response.use(
+    function (response) {
+        return response;
+    },
+    async function (error) {
+        if (error.response.status === 419) {
+            await AuthApi.cookie();
+
+            return axiosInstance.request(error.config);
+        }
+
+        return Promise.reject(error);
+    }
+);
