b81d34791d5cf378ae99133cd46e83791b300007
Jakub Mrówczyński <jmrowczynski12@gmail.com>
2023-04-12 22:29:23 +0200

    add product types

---
+++ src/services/api/types/product.ts
@@ -0,0 +1,34 @@
+export interface IProductsResponse {
+    current_page: number;
+    data: IProduct[];
+    first_page_url: string;
+    from: number;
+    last_page: number;
+    last_page_url: string;
+    links: Link[];
+    next_page_url: string;
+    path: string;
+    per_page: number;
+    prev_page_url: any;
+    to: number;
+    total: number;
+}
+
+export interface IProduct {
+    id: number;
+    created_at: string;
+    updated_at: string;
+    name: string;
+    price: number;
+    quantity: number;
+}
+
+export interface Link {
+    url?: string;
+    label: string;
+    active: boolean;
+}
+
+export interface IProductsParams {
+    page?: number;
+}
