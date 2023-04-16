32bd6403ae65b8e336e65416f6d30d02280cd64d
Jakub Mrówczyński <jmrowczynski12@gmail.com>
2023-04-17 00:08:57 +0200

    create input filter

---
+++ src/components/Filters/InputFilter.vue
@@ -0,0 +1,29 @@
+<template>
+    <input
+        class="input input-bordered input-primary w-full"
+        :type="type"
+        :placeholder="placeholder"
+        :aria-label="ariaLabel"
+        :name="name"
+        @input="onChange"
+    />
+</template>
+
+<script setup lang="ts">
+interface Props {
+    type?: string;
+    placeholder?: string;
+    ariaLabel?: string;
+    name: string;
+}
+
+const emit = defineEmits<{ (e: 'onChange', value: string): void }>();
+
+withDefaults(defineProps<Props>(), {
+    type: 'text',
+});
+
+const onChange = (event: InputEvent) => {
+    emit('onChange', (event.target as HTMLInputElement).value);
+};
+</script>
