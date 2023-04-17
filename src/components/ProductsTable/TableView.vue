6ad62a76a278b07fa4d637f2aa5d52d688f350b3
Jakub Mrówczyński <jmrowczynski12@gmail.com>
2023-04-17 23:50:55 +0200

    add products table actions buttons

--- src/components/ProductsTable/TableView.vue
+++ src/components/ProductsTable/TableView.vue
@@ -1,10 +1,28 @@
 <template>
-    <TableComponent :head="['Nazwa', 'Cena', 'Ilość', 'Wartość razem']">
+    <TableComponent
+        :head="['Nazwa', 'Cena', 'Ilość', 'Wartość razem', 'Akcje']"
+    >
         <tr :key="item.id" v-for="item in data" class="hover">
             <td>{{ item.name }}</td>
-            <td>{{ item.price }}</td>
+            <td>{{ item.price.toFixed(2) }}</td>
             <td>{{ item.quantity }}</td>
             <td>{{ (item.price * item.quantity).toFixed(2) }}</td>
+            <td>
+                <div class="flex gap-2">
+                    <button
+                        class="text-error hover:text-opacity-80"
+                        title="Usuń produkt"
+                    >
+                        <TrashIcon width="20" height="20" />
+                    </button>
+                    <button
+                        class="text-accent hover:text-opacity-80"
+                        title="Edytuj produkt"
+                    >
+                        <PencilIcon width="20" height="20" />
+                    </button>
+                </div>
+            </td>
         </tr>
     </TableComponent>
 </template>
@@ -12,6 +30,8 @@
 <script setup lang="ts">
 import { IProductsResponse } from '@/services/api/types/product';
 import TableComponent from '@components/TableComponent/TableComponent.vue';
+import TrashIcon from '@assets/icons/trash.svg';
+import PencilIcon from '@assets/icons/pencil.svg';
 
 defineProps<{ data: IProductsResponse['data'] }>();
 </script>
