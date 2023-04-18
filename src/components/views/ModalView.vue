bebb6dadde800c1b6fb28670d6c21f34bbe1d484
Jakub Mrówczyński <jmrowczynski12@gmail.com>
2023-04-18 22:05:29 +0200

    create modal component

---
+++ src/components/views/ModalView.vue
@@ -0,0 +1,48 @@
+<template>
+    <Teleport to="#modals">
+        <div class="modal" :class="{ 'modal-open': isOpen }">
+            <div class="modal-box relative" ref="modalBoxRef">
+                <button
+                    class="btn btn-sm btn-circle absolute right-2 top-2"
+                    @click="onCloseModal"
+                >
+                    ✕
+                </button>
+                <h2 class="text-lg font-bold mb-4">
+                    {{ title }}
+                </h2>
+                <p class="mb-4" v-if="description" v-html="description"></p>
+                <footer class="flex gap-2 justify-end">
+                    <button class="btn" @click="onCloseModal">Nie</button>
+                    <button class="btn btn-error">Tak</button>
+                </footer>
+            </div>
+        </div>
+    </Teleport>
+</template>
+
+<script setup lang="ts">
+import { onClickOutside } from '@vueuse/core';
+import { ref } from 'vue';
+
+interface Props {
+    title: string;
+    description?: string;
+    isOpen: boolean;
+}
+defineProps<Props>();
+
+const modalBoxRef = ref(null);
+
+const emit = defineEmits<{ (e: 'onCloseModal'): void }>();
+
+const onCloseModal = () => emit('onCloseModal');
+
+onClickOutside(modalBoxRef, onCloseModal);
+</script>
+
+<script lang="ts">
+export default {
+    name: 'ModalView',
+};
+</script>
